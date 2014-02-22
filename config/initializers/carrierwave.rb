5df2715bf9d4152111c9fd55d815afe79ac8c9a6
Calvin Lam <calvinwlam@gmail.com>
2014-02-22 09:41:30 -0800

    it uploads and displays from amazon, used carrierwave-aws gem config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = 'goodevil'
   config.aws_acl    = :public_read
-  config.asset_host = 'https://s3-us-west-1.amazonaws.com'
+  config.asset_host = 'https://s3.amazonaws.com/goodevil'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
   config.aws_credentials = {
-    access_key_id:     'AKIAI6AECUXY23A6B56Q',
-    secret_access_key: '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'
+    access_key_id:     ENV['S3_KEY'],
+    secret_access_key: ENV['S3_SECRET']
   }
 end
