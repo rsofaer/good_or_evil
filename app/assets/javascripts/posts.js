aada823504568fd8cbc4df9b44027c680b2d2156
jongmini <jongmin.choi@gmail.com>
2014-02-23 22:45:42 -0800

    working on creating the like object to send with ajax call

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,19 +1,23 @@
 $(function(){
 
-  $('.item').on('click','.comments', function(event){
+  $('.item').on('click','.post', function(event){
   console.log(event);
   console.log(event.target);
   if(event.target.id === "good"){
     // var checkbox = event.target;
     var good_btn = event.target;
     var _this = this;
-    console.log("GOOD!");
     console.log(this);
-    // console.log(this.dataset);
+    console.log(this.dataset);
     // var updated_todo = {};
-    var good = {};
+    var good = {good: true, likeable_type:"Post"};
+    good.likeable_id = this.dataset.id;
+
+    console.log(good);
+
+
   //   updated_todo.completed = checkbox.checked;
-    // updated_todo.id = this.dataset.id;
+    // good.id = this.dataset.id;
 
   //   // Let's write a update request
   //   $.ajax(
@@ -21,6 +25,21 @@ $(function(){
   //   }).done(function(data){
   //       $(_this).toggleClass("done-true");
   //     });
+    }
+
+  });
+
+  $('.item').on('click','.comment', function(event){
+  console.log(event);
+  console.log(event.target);
+  if(event.target.id === "good"){
+    // var checkbox = event.target;
+    var good_btn = event.target;
+    var _this = this;
+    console.log(this);
+    console.log(this.dataset);
+    var good = {};
+
     }
 
   });
