3e4a13001580e4ab3b58387feab0ee4656cf2c9f
ydarbleoj <brady.joel@gmail.com>
2014-02-26 11:46:12 -0800

    link issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :new_post 
+
+  def new_post
+    @post = Post.new
+  end 
+
 end
