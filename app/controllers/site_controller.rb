3179a994d7d4fc3b17821bc29e4b1b80e8e36ac7
jongmini <jongmin.choi@gmail.com>
2014-02-27 11:51:35 -0800

    donut graph works in profile page. bruce to take over

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,11 @@ class SiteController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    gon.post_count = @user.posts.count
+    gon.good_like_count = @user.likes.where(good:true).count
+    gon.evil_like_count = @user.likes.where(good:false).count
+    gon.comment_count = @user.comments.count
+    gon.good_like_total = Like.where(good:true).count
   end
   
 end
