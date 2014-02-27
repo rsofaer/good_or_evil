017316e151d3911d988282c4e310dd337f30e2fa
ydarbleoj <brady.joel@gmail.com>
2014-02-27 12:33:48 -0800

    landing page stuff

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,19 @@
 class SiteController < ApplicationController
-  
+      
+  def index
+    
+    # if current_user
+    #   if current_user.nil?
+    #     redirect_to root_path
+    #   else
+    #     redirect_to posts_path
+    #   end
+    # end
+  end
+
   def show
     @user = User.find(params[:id])
   end
   
 end
+ 
