2f6ed6e921354b3187d0e421ab86bbd9a1a5a24a
brucetoddlee <brucetlee@gmail.com>
2014-02-27 18:15:44 -0800

    added user profile charts

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -7,11 +7,11 @@ window.onload = function () {
                 ]);
 
 
-var chart = new CanvasJS.Chart("chartContainer_profile",
+  var chart1 = new CanvasJS.Chart("chartContainer_all_votes_all_posts",
     {
       animationEnabled: true, // change to false
       title:{
-        text: "My Good vs Evil Vote"
+        text: "Everyone's votes on everyone's posts"
       },
       backgroundColor: "none",
       colorSet: "goodorevil",
@@ -20,14 +20,58 @@ var chart = new CanvasJS.Chart("chartContainer_profile",
       {
         type: "doughnut",
        dataPoints: [
-        { label: "good", y: gon.good_like_count },
-        { label: "evil", y: gon.evil_like_count }
+        { label: "good", y: gon.all_votes_all_posts_good },
+        { label: "evil", y: gon.all_votes_all_posts_evil }
        ]
      }
      ]
    });
 
-    chart.render();
+    chart1.render();
+
+  var chart2 = new CanvasJS.Chart("chartContainer_your_votes_all_posts",
+    {
+      animationEnabled: true, // change to false
+      title:{
+        text: "Your votes on everyone's posts"
+      },
+      backgroundColor: "none",
+      colorSet: "goodorevil",
+      creditText: "",
+      data: [
+      {
+        type: "doughnut",
+       dataPoints: [
+        { label: "good", y: gon.your_votes_all_posts_good },
+        { label: "evil", y: gon.your_votes_all_posts_evil }
+       ]
+     }
+     ]
+   });
+
+    chart2.render();
+
+    var chart3 = new CanvasJS.Chart("chartContainer_all_votes_your_posts",
+    {
+      animationEnabled: true, // change to false
+      title:{
+        text: "Everyone's votes on your posts"
+      },
+      backgroundColor: "none",
+      colorSet: "goodorevil",
+      creditText: "",
+      data: [
+      {
+        type: "doughnut",
+       dataPoints: [
+        { label: "good", y: gon.all_votes_your_posts_good },
+        { label: "evil", y: gon.all_votes_your_posts_evil }
+       ]
+     }
+     ]
+   });
+
+    chart3.render();
 
     
 };
